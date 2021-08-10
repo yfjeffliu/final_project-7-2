d420c594247a5c0b6cecd6a4d4337a49b8cc1abc
KionGao <87817891+KionGao@users.noreply.github.com>
2021-08-10 22:26:08 +0800

    Add files via upload

---
+++ music/music.py
@@ -0,0 +1,28 @@
+#音樂
+import pygame
+from pygame import mixer
+
+pygame.mixer.init
+pygame.mixer.music.load("music","bg_music.wav")
+
+
+class Music:
+    def __init__(self, subject):
+        subject.register(self)
+
+    def update(self, user_request: str, model):
+        """music on"""
+        if user_request == "music":
+            pygame.mixer.music.unpause()
+            model.sound.play()
+
+
+class Muse:
+    def __init__(self, subject):
+        subject.register(self)
+
+    def update(self, user_request: str, model):
+        """music off"""
+        if user_request == "mute":
+            pygame.mixer.music.pause()
+            model.sound.play()
